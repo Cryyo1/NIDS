fde3aa67ebbd7e051b35fc4b931af777df25f4a0
Chakib-Boudjema <chakibboudjema@gmail.com>
2022-12-05 18:44:50 +0100

    progressbar

--- src/components/Notification/NotificationCard.jsx
+++ src/components/Notification/NotificationCard.jsx
@@ -2,7 +2,7 @@
 
 const NotificationCard = ({ icon, title, subTitle, description, time }) => {
 	return (
-		<div class="grid grid-rows-3 grid-flow-col gap-4 p-7 m-4 rounded-md ..." style={{ backgroundColor: "#38363c" }}>
+		<div class="grid grid-rows-3 grid-flow-col gap-4 p-7 m-4  bg-lightgrey rounded-md ...">
 			<div className="col-span-1 row-span-1 ..."><p className="text-white text-lg">{icon}</p></div>
 			<div className="col-span-3 ..."><p className="text-white text-base">{subTitle}</p></div>
 			<div className="col-span-3 ..."><p className="text-white text-sm">{description}</p></div>
